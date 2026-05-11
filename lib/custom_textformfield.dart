444cd452e6440a51dfa7040e127c2a66fccdb4b4
Radwa <radwamahmoud816@gmail.com>
2026-05-11 21:11:14 +0300

    custome_textformfield

---
+++ lib/custom_textformfield.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String title;
+  final bool isRequired;
+  final String hint;
+  final IconData? hinticon;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  const CustomTextFormField({
+    super.key,
+    required this.title,
+    this.isRequired = false,
+    required this.hint,
+    this.hinticon,
+    required this.controller,
+    this.validator,
+    this.obscureText = false,
+    required this.keyboardType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              if (isRequired)
+                const Icon(Icons.star, color: Colors.red, size: 10),
+              const SizedBox(width: 8),
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          TextFormField(
+            controller: controller,
+            validator: validator,
+            obscureText: obscureText,
+            keyboardType: keyboardType,
+
+            decoration: InputDecoration(
+              hint: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(hint, style: const TextStyle(color: Colors.grey)),
+                  SizedBox(width: 8),
+                  Icon(hinticon, color: Colors.grey, size: 12),
+                ],
+              ),
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 10,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

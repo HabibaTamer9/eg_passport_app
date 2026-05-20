780527c47407ca49dedaeccb8ae77f6069744d8f
sohilaEmam2003 <shyltamam@gmail.com>
2026-05-20 04:11:01 +0300

    register_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
-import 'package:easy_localization/easy_localization.dart';
-import 'package:eg_passport_app/login_screen/login_screen.dart';
+import 'package:easy_localization/easy_localization.dart' show EasyLocalization, BuildContextEasyLocalizationExtension;
+import 'package:eg_passport_app/LoginScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'register/register_screen.dart';
+import 'theme/my_theme_app.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
   runApp(
     EasyLocalization(
-        supportedLocales: [Locale('en'), Locale('ar')],
-        path: 'assets/translations', // <-- change the path of the translation files
-        fallbackLocale: Locale('en',),
-        child: MyApp()
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+      ],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('ar'),
+      startLocale: const Locale('ar'), // 🔥 مهم جدًا
+      child: const MyApp(),
     ),
   );
 }
@@ -20,22 +26,40 @@ void main() async{
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(360, 840),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        // Use builder only if you need to use library outside ScreenUtilInit context
-        builder: (_ , child) {
+      designSize: const Size(360, 840),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
         return MaterialApp(
-          localizationsDelegates: context.localizationDelegates,
-          supportedLocales: context.supportedLocales,
+          debugShowCheckedModeBanner: false,
+
+          theme: MyThemeApp.lightTheme,
+
+          // 🔥 localization
           locale: context.locale,
-          home: LoginScreen(),
+          supportedLocales: context.supportedLocales,
+          localizationsDelegates: context.localizationDelegates,
+
+          // 🔥 الاتجاه الصح (RTL/LTR)
+          builder: (context, widget) {
+            return Directionality(
+              textDirection: context.locale.languageCode == 'ar'
+                  ? TextDirection.rtl
+                  : TextDirection.ltr,
+              child: widget ?? const SizedBox(),
+            );
+          },
+
+          home: const RegisterScreen(),
+            routes: {
+              RegisterScreen.routeName: (context) => const RegisterScreen(),
+             Loginscreen.routeName: (context) => const Loginscreen(),
+            }
         );
-      }
+      },
     );
   }
 }

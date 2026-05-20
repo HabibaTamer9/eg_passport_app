c472022a4e8080a957676ad640e24fcebdf53651
aya <ayai89116@gmail.com>
2026-05-20 10:59:54 +0300

    fix: update main.dart layout to view document upload screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:eg_passport_app/login_screen/login_screen.dart';
+//import 'package:eg_passport_app/login_screen/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:eg_passport_app/document_upload.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
           localizationsDelegates: context.localizationDelegates,
           supportedLocales: context.supportedLocales,
           locale: context.locale,
-          home: LoginScreen(),
+          home: const DocumentUploadScreen(),
         );
       }
     );
